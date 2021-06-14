929c9d453aed72817d4510ad7971715d8de1cd1e
Yohenba18 <roddik12kshetri@gmail.com>
2021-06-14 23:22:40 +0530

    components sidebar and chat added

---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,12 @@
+import firebaseConfig from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCYWdHjSPyR0bh8tkj3g9ZyPs9EUCYFErY",
+    authDomain: "whatsapp-mern-b37d8.firebaseapp.com",
+    projectId: "whatsapp-mern-b37d8",
+    storageBucket: "whatsapp-mern-b37d8.appspot.com",
+    messagingSenderId: "268800542694",
+    appId: "1:268800542694:web:b237a191ab83a685bd7827"
+  };
+
+  expect  firebaseConfig;
