2a614cbf93ceda869e132bf1e33440234848c82e
Yohenba18 <roddik12kshetri@gmail.com>
2021-06-16 22:30:01 +0530

    search

--- whatsapp/src/components/Sidebar.js
+++ whatsapp/src/components/Sidebar.js
@@ -4,6 +4,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 
 function Sidebar() {
   return (
@@ -23,7 +24,10 @@ function Sidebar() {
         </div>
       </div>
       <div className="sidebar__search">
-      
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon/>
+          <input type="text" placeholder="Search or start a new chat"/>
+        </div>
       </div>
     </div>
   );
