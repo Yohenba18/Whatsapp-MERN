987c6168dfeff6b93239a23af2ec1e1ff75f8a8a
Yohenba18 <roddik12kshetri@gmail.com>
2021-06-16 21:42:27 +0530

    avatar

--- whatsapp/src/components/Sidebar.js
+++ whatsapp/src/components/Sidebar.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
-    return (
-        <div className= "sidebar"> 
-            <h1>Sidebar</h1>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="http://cdn.epicstream.com/assets/uploads/ckeditor/images/1612289466_sub-buzz-11718-1591678685-12.png"/>
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
