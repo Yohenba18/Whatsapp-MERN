7fa3bf689310aa3e3b0007fb93ef6e75b095578a
Yohenba18 <roddik12kshetri@gmail.com>
2021-06-24 19:12:15 +0530

    post bug

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -21,29 +21,26 @@ app.use(express.json());
 const connection_url =
   "mongodb+srv://admin:JqnmBfV9dxUYmZ2U@cluster0.rmpgm.mongodb.net/whatsappdb?retryWrites=true&w=majority";
 
-
 mongoose.connect(connection_url, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // api routes
-app.get("/", (req, res) => {
-  res.status(200).send("Hello world");
-});
+app.get("/", (req, res) => res.status(200).send("Hello World"));
 
-app.post('/messages/new', (req, res) => {
+app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
 
   Messages.create(dbMessage, (err, data) => {
-      if (err) {
-          res.status(500).send(err);
-      }else{
-          res.status(201).send(data);
-      }
-  })
-})
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
 //listeners
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
