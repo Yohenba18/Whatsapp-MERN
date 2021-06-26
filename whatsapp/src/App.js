47c50adba2a473baa30b0fe1c622550bc77d6cce
Yohenba18 <roddik12kshetri@gmail.com>
2021-06-26 10:31:35 +0530

    added pusher to the frontend

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
+import Pusher from "pusher";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("3e00697874ce96ac8590", {
+      cluster: "ap2",
+    });
 
-  
+    const channel = pusher.subscribe("my-channel");
+    channel.bind("my-event", function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
   return (
     <div className="app">
       <div className="app__body">
