c8ece1836c0c5cb133a605465462ad64eb515ca4
Yohenba18 <roddik12kshetri@gmail.com>
2021-06-26 11:13:36 +0530

    real time messages

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
 import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-
-  },[]);
+    axios.get("/messages/sync").then((response) => {
+      // console.log(response.data);
+      setMessages(response.data);
+    });
+  }, []);
 
   useEffect(() => {
     const pusher = new Pusher("3e00697874ce96ac8590", {
@@ -16,10 +21,12 @@ function App() {
     });
 
     const channel = pusher.subscribe("messages");
-    channel.bind("inserted", (data) =>  {
+    channel.bind("inserted", (data) => {
       alert(JSON.stringify(data));
     });
   }, []);
+
+  console.log(messages);
   return (
     <div className="app">
       <div className="app__body">
